59f7cd85074fec75f288bdb0eb060c07d3440050
Nicholas Lee <nick.lee@berkeley.edu>
2023-06-22 08:00:04 +0000

    cleaned database

--- scripts/base.py
+++ scripts/base.py
@@ -1,11 +1,23 @@
-import os
 import yaml
-import pandas as pd
-import numpy as np
+from sqlalchemy.engine import URL
+from sqlalchemy import create_engine
 
 # set up
 with open("/home/ubuntu/work/therapeutic_accelerator/config/main.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
     
-with open("../config/keys.yaml", "r") as f:
+with open("/home/ubuntu/work/therapeutic_accelerator/config/keys.yaml", "r") as f:
     keys = yaml.load(f, Loader=yaml.FullLoader)
+    
+url_object = URL.create(
+    'postgresql', 
+    username='postgres',
+    password=keys["postgres"], 
+    host=config["database"]["host"],
+    database='postgres',
+    port=5432
+)
+
+# Create engine to connect to database
+# psql_string = f'postgresql://postgres:{keys["postgres"]}@{config["database"]["host"]}:5432/postgres'
+engine = create_engine(url_object)
