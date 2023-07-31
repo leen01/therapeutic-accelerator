09b325d5d77554e90ffe194d998c4d839167cb73
Nicholas Lee <nick.lee@berkeley.edu>
2023-07-31 04:54:48 +0000

    created background job for upload

--- custom_packages/utils/upload_abstracts_specter.py
+++ custom_packages/utils/upload_abstracts_specter.py
@@ -1,37 +1,21 @@
-#! usr/bin/env python
+#! usr/bin/env python3
 import warnings
-import tensorflow as tf
-import os
-import torch
-from transformers import AutoModel, AutoTokenizer
-from dask.diagnostics import ProgressBar
-from dask import distributed, dataframe as dd
-from dask.distributed import TimeoutError, KilledWorker
 import dask
-from tqdm.notebook import tqdm_notebook
-from tqdm import tqdm
-import ast
-import re
-import logging
+from dask.distributed import LocalCluster, Client, TimeoutError, KilledWorker
 import numpy as np
 import pandas as pd
-import sys
-
-
-# sys.path.append(
-#     "/home/ubuntu/work/therapeutic_accelerator/custom_packages/utils")
-# sys.path.append(
-#     "/home/ubuntu/work/therapeutic_accelerator/custom_packages/database")
-
-from utils import import_config
-from db_tools import db_connection
+from tqdm import tqdm
+from transformers import AutoModel, AutoTokenizer
 import chromaDB as cbd  # specter_ef, create_text_splitter, create_chroma_client
+from db_tools import db_connection
+from utils import import_config
 
 # hide warnings
+warnings.filterwarnings("ignore")
 
-def remove_exisiting(df):
-    """ Remove existing ids from dataframe """
-    # get unique ids from collection that are the corpus ids
+
+def remove_existing(df, collection):
+    """ Remove existing ids from the dataframe """
     existing_ids = np.unique(pd.DataFrame(collection.get(include=['metadatas']))['ids'].str.split(
         '-', expand=True).rename(columns={0: 'id', 1: 'part'})['id'].tolist()).tolist()
     print('N existing ids: ', len(existing_ids))
@@ -43,33 +27,24 @@ def remove_exisiting(df):
 
     return df
 
+
 def create_ids_metadatas(corpusid, num_chunks):
     """ Create ids for each chunk """
-
     ids = [f"{corpusid}-{i}" for i in range(num_chunks)]
-
     metadatas = [{'corpusid': corpusid, 'chunk': i} for i in range(num_chunks)]
-
     return ids, metadatas
 
-
-def process_dict(d):
-
+def process_dict(d, specter_embeder, collection):
     corpusid = d['corpusId']
-
     documents = specter_embeder.split_text(d['documents'])
-
     ids, metadatas = create_ids_metadatas(corpusid, len(documents))
-
     embeddings = specter_embeder.embed_documents(documents)
-
     new_dict = {
         'ids': ids,
         'documents': documents,
         'metadatas': metadatas,
         'embeddings': embeddings
     }
-
     try:
         collection.upsert(**new_dict)
         return True
@@ -78,101 +53,34 @@ def process_dict(d):
 
 
 if __name__ == "__main__":
-    
-    # Disable TensorFlow INFO and WARNING messages
-    # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    
-    # tf.get_logger().setLevel('ERROR')  # Disable TensorFlow ERROR messages (optional)
+    # ... (remaining imports)
 
-    # Filter out Dask client warnings
-    warnings.filterwarnings("ignore")
-
-    config, keys = import_config()
-
-    # Chroma setup --------------------------------------------------------------------------------
-    chroma_server_host = "3.86.216.30"
-    
+    # Chroma setup
+    chroma_server_host = "3.88.178.230"
     chroma_client = cbd.create_chroma_client(chroma_server_host)
 
-    # Modelish stuff --------------------------------------------------------------------------------
-
+    # Modelish stuff
     model = AutoModel.from_pretrained("allenai/specter")
-    
     tokenizer = AutoTokenizer.from_pretrained("allenai/specter")
-
-    # Create embeddings function with specter model
     specter_embeder = cbd.specter_ef(model, tokenizer)
-
     specter_embeder.create_text_splitter()
 
     # Working Collection
     collection = chroma_client.get_or_create_collection(
         "specter_abstracts", embedding_function=specter_embeder)
     
+    print(collection.count())
+
     abstracts = pd.read_csv("/home/ubuntu/work/data/abstracts.csv")
+    
+    abstracts = remove_existing(abstracts, collection)
 
-    abstracts = remove_exisiting(abstracts)
-
-    # # Dask functions
-    # Create dask cluster
-    # overwrite default with multiprocessing scheduler
-    dask.config.set(scheduler='processes')
-
-    # Launches a scheduler and workers locally
-    cluster = distributed.LocalCluster(
-        name='local', n_workers=5,
-        memory_limit='5GiB',
-        threads_per_worker=1, 
-        retries=10)
-
-    client = distributed.Client(cluster, ip='http://127.0.0.1:8787')
-
-    print('-' * 20)
-    print(client)
-    print('-' * 20)
-
-    # # Add documents to collection
-
-    # ab_tester = abstracts.iloc[:10000, :]
+    # Add documents to collection
     n_partitions = len(abstracts) // 100
+    
     print(n_partitions)
 
     abstracts_dict = abstracts.to_dict('records')
-
-    chunked_abstracts = np.array_split(abstracts_dict, n_partitions)
-
-    partition_size = 10
-
-    for i in tqdm(range(len(chunked_abstracts)), desc='chunk'):
-
-        dict_chunk = chunked_abstracts[i]
-
-        futures = [dask.delayed(process_dict)(d) for d in dict_chunk]
-
-        num_partitions = len(futures)//partition_size
-
-        futures_split = np.array_split(futures, num_partitions)
-
-        for j in tqdm(range(len(futures_split)), leave=True, desc='partition'):
-
-            fs = futures_split[j]
-            try: 
-                results = dask.compute(*fs, optimize_graph=True)
-                del results
-                
-            except TimeoutError:
-                print('TimeoutError')
-                continue
-            
-            except KilledWorker as e:
-                # Handle the KilledWorker error here
-                # You can perform cleanup, logging, or take appropriate action
-                print("A Dask worker was killed:", e)
-                pass
-            
-        client.restart()
-
-        del dict_chunk, futures, futures_split
-
-    # Optionally, you can close the Dask client after your computation is done
-    client.close()
+    
+    for d in tqdm(range(len(abstracts_dict)), desc='abstracts'):
+        process_dict(abstracts_dict[d], specter_embeder, collection)
