e2aaf38307518d7079338059836c534c6f4f7f03
Nicholas Lee <nick.lee@berkeley.edu>
2023-08-02 15:08:38 +0000

    created scripts for uploading text to chroma

--- custom_packages/utils/upload_abstracts_specter.py
+++ custom_packages/utils/upload_abstracts_specter.py
@@ -1,7 +1,5 @@
 #! usr/bin/env python3
 import warnings
-import dask
-from dask.distributed import LocalCluster, Client, TimeoutError, KilledWorker
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -9,51 +7,65 @@ from transformers import AutoModel, AutoTokenizer
 import chromaDB as cbd  # specter_ef, create_text_splitter, create_chroma_client
 from db_tools import db_connection
 from utils import import_config
+import logging
 
 # hide warnings
 warnings.filterwarnings("ignore")
 
+def remove_exisiting(df, collection):
+    """ Remove existing ids from dataframe """
+    # get unique ids from collection that are the corpus ids
+    query_filter = {
+        "$or": [{'corpusid': str(c)} for c in df['corpusid'].unique()]
+    }
 
-def remove_existing(df, collection):
-    """ Remove existing ids from the dataframe """
-    existing_ids = np.unique(pd.DataFrame(collection.get(include=['metadatas']))['ids'].str.split(
-        '-', expand=True).rename(columns={0: 'id', 1: 'part'})['id'].tolist()).tolist()
-    print('N existing ids: ', len(existing_ids))
-    print(df.shape)
+    # get ids in df from collection
+    temp = collection.get(
+        include=["metadatas"],
+        where=query_filter
+    )
 
-    df = df[~df['corpusId'].astype(str).isin(existing_ids)]
+    if len(temp['ids']) > 0: 
+        test_ids = pd.DataFrame(temp['metadatas'])
 
-    print(df.shape)
+        existing_ids = test_ids.astype(str)['corpusid'].unique()
 
+        df = df[~df['corpusid'].astype(str).isin(existing_ids)]
+        
     return df
 
 
-def create_ids_metadatas(corpusid, num_chunks):
-    """ Create ids for each chunk """
-    ids = [f"{corpusid}-{i}" for i in range(num_chunks)]
-    metadatas = [{'corpusid': corpusid, 'chunk': i} for i in range(num_chunks)]
-    return ids, metadatas
+# @dask.delayed
+def create_split_text(df): 
+    
+    df['documents'] = df.apply(lambda x: specter_embeder.split_text(x['documents']), axis=1)
+    
+    return df
 
-def process_dict(d, specter_embeder, collection):
-    corpusid = d['corpusId']
-    documents = specter_embeder.split_text(d['documents'])
-    ids, metadatas = create_ids_metadatas(corpusid, len(documents))
-    embeddings = specter_embeder.embed_documents(documents)
-    new_dict = {
-        'ids': ids,
-        'documents': documents,
-        'metadatas': metadatas,
-        'embeddings': embeddings
-    }
-    try:
-        collection.upsert(**new_dict)
-        return True
-    except:
-        return False
+def create_ids(df): 
+    """ Create ids for each chunk """
+    
+    df['ids'] = df.apply(lambda x: [f"{x['corpusid']}-{i}" for i in range(len(x['documents']))], axis=1)
+    
+    return df
+
+def create_metadatas(df): 
+    """ Create metadatas for each chunk """
+    
+    df['metadatas'] = df.apply(lambda x: [{'corpusid': x['corpusid'], 'chunk':i} for i in range(len(x['documents']))], axis=1)
+    
+    return df
+
+def create_embeddings(df): 
+    
+    df['embeddings'] = df.apply(lambda x: specter_embeder.embed_documents(x['documents']), axis=1)
+    
+    return df
 
 
 if __name__ == "__main__":
     # ... (remaining imports)
+    logging.basicConfig(filename='/home/ubuntu/work/therapeutic_accelerator/logs/abstract_embeddings_dask.log', level=logging.ERROR)
 
     # Chroma setup
     chroma_server_host = "3.88.178.230"
@@ -69,18 +81,56 @@ if __name__ == "__main__":
     collection = chroma_client.get_or_create_collection(
         "specter_abstracts", embedding_function=specter_embeder)
     
-    print(collection.count())
+    print('Collection count: ', collection.count())
 
     abstracts = pd.read_csv("/home/ubuntu/work/data/abstracts.csv")
-    
-    abstracts = remove_existing(abstracts, collection)
 
+    abstracts = abstracts.rename({'corpusId':'corpusid'}, axis = 1)
+    
     # Add documents to collection
     n_partitions = len(abstracts) // 100
     
-    print(n_partitions)
-
-    abstracts_dict = abstracts.to_dict('records')
+    print('Partitions: ', n_partitions)
     
-    for d in tqdm(range(len(abstracts_dict)), desc='abstracts'):
-        process_dict(abstracts_dict[d], specter_embeder, collection)
+    abstracts_split = np.array_split(abstracts, n_partitions)
+
+    for i in tqdm(range(len(abstracts_split)), desc="Processing abstract partitions"):
+        abstracts_split[i] = remove_exisiting(abstracts_split[i], collection)
+        if len(abstracts_split[i]) > 0: 
+            df = create_split_text(abstracts_split[i])
+            df = create_embeddings(df)
+            df = create_ids(df)
+            df = create_metadatas(df)
+            df = df.drop(columns = ['corpusid'])
+            dict_series = df.to_dict('records')
+            
+            temp_dict = {
+                'documents': [], 
+                'ids': [], 
+                'embeddings': [], 
+                'metadatas': []
+            }
+
+            # combine dictionaries into one
+            for ds in dict_series:
+                for k in temp_dict.keys(): 
+                    temp_dict[k] = temp_dict[k] + ds[k]
+                    
+            try: 
+                collection.add(**temp_dict)
+                print('Collection count: ', collection.count())
+            except Exception as e: 
+                logging.exception("Error occurred while adding documents to collection")
+                # back up abstracts
+                abstracts.to_csv("/home/ubuntu/work/data/abstracts_backup.csv", index = False)
+                
+                # create new csv so chroma does not have to start from beginning
+                pd.concat(abstracts_split).to_csv("/home/ubuntu/work/data/abstracts.csv", index = False)
+        else: 
+            print('No new abstracts to add to collection')
+            abstracts.to_csv("/home/ubuntu/work/data/abstracts_backup.csv", index = False)
+            
+            # create new csv so chroma does not have to start from beginning
+            pd.concat(abstracts_split).to_csv("/home/ubuntu/work/data/abstracts.csv", index = False)
+        
+            
